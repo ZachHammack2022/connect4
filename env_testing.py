17ed5736a0635ad59d5ee2799e9dada8661ffaad
Zach Hammack <zhammack@princeton.edu>
2023-12-22 22:23:23 -0600

    adds unit tests, edits environment for rl training

---
+++ env_testing.py
@@ -0,0 +1,85 @@
+import unittest
+from game import Connect4Env
+
+class TestConnect4Env(unittest.TestCase):
+
+    def test_horizontal_win(self):
+        env = Connect4Env()
+        moves = [1, 1, 2, 2, 3, 3, 4]
+        last_player = 'X'  # Start with player 'X'
+
+        for move in moves:
+            _, _, done, _ = env.step(move)
+            if done:
+                break
+            last_player = 'O' if last_player == 'X' else 'X'  # Switch player after each move
+
+        self.assertTrue(env.check_winner(last_player))  # Check if the last player won
+
+    # Test for vertical win
+    def test_vertical_win(self):
+        env = Connect4Env()
+        moves = [0, 1, 0, 1, 0, 1, 0]  # Same column to get a vertical win
+        last_player = 'X'
+
+        for move in moves:
+            _, _, done, _ = env.step(move)
+            if done:
+                break
+            last_player = 'O' if last_player == 'X' else 'X'
+        
+        self.assertTrue(env.check_winner(last_player))
+
+    # Test for positive diagonal win
+    def test_positive_diagonal_win(self):
+        env = Connect4Env()
+        moves = [0, 1, 1, 2, 2, 3, 2, 3, 5, 3,3]  # Moves to create a positive diagonal
+        last_player = 'X'
+
+        for move in moves:
+            _, _, done, _ = env.step(move)
+            if done:
+                break
+            last_player = 'O' if last_player == 'X' else 'X'
+        self.assertTrue(env.check_winner(last_player))
+
+    # Test for negative diagonal win
+    def test_negative_diagonal_win(self):
+        env = Connect4Env()
+        moves = [3, 2, 2, 1, 1, 0, 1, 0, 0, 6,0]  # Moves to create a negative diagonal
+        last_player = 'X'
+
+        for move in moves:
+            _, _, done, _ = env.step(move)
+            if done:
+                break
+            last_player = 'O' if last_player == 'X' else 'X'
+        self.assertTrue(env.check_winner(last_player))
+
+    # Test for a full board without a winner
+    def test_full_board_no_winner(self):
+        env = Connect4Env()
+        # A sequence of moves that fills the board without a winner
+        moves = [
+            1, 0, 0,0,0,0, 0, # Column 0
+            4,6,1,1,1,1, #column 1
+            2,2,2,2,2,2, #etc
+            3,3,3,3,3,3,
+            4,4,4,4,4,
+            5,5,5,5,5,5,
+            6,6,6,6,6,1
+        ]
+        
+        for move in moves:
+            _, _, done, _ = env.step(move)
+        self.assertTrue(env._is_board_full() and not env.check_winner('X') and not env.check_winner('O'))
+
+
+    def test_invalid_move(self):
+        env = Connect4Env()
+        env.board[0][0] = 'X'  # Column 0 is now full
+        self.assertFalse(env._is_valid_action(0))
+
+
+if __name__ == '__main__':
+    unittest.main()
--- game.py
+++ game.py
@@ -1,8 +1,77 @@
-class Connect4:
+import gym
+from gym import spaces
+import random
+
+class Connect4Env(gym.Env):
     def __init__(self):
+        super(Connect4Env, self).__init__()
         self.board = [[' ' for _ in range(7)] for _ in range(6)]
         self.current_player = 'X'
-        self.game_over = False
+        self.action_space = spaces.Discrete(7)  # 7 columns
+        self.observation_space = spaces.Box(low=0, high=2, shape=(6, 7), dtype=int)
+
+    def step(self, action):
+        # Check if action is valid
+        if not self._is_valid_action(action):
+            return self._get_obs(), -10, True, {"msg": "Invalid action, column full."}
+
+        player_who_moved = self.current_player
+        self._make_move(action)
+        
+        # Check if the move resulted in a win
+        won = self.check_winner(player_who_moved)
+        done = won or self._is_board_full()
+    
+        # If the game is won, reward should be 1, otherwise 0
+        reward = 1 if won else 0
+
+        return self._get_obs(), reward, done, {}
+
+
+    def reset(self):
+        self.board = [[' ' for _ in range(7)] for _ in range(6)]
+        self.current_player = 'X'
+        return self._get_obs()
+
+    def render(self, mode='human', close=False):
+        if close:
+            return
+        print(" 0 1 2 3 4 5 6")
+        for row in self.board:
+            print('|' + '|'.join(row) + '|')
+
+    def close(self):
+        pass
+
+    # Additional methods (private)
+    def _make_move(self, column):
+        for row in reversed(self.board):
+            if row[column] == ' ':
+                row[column] = self.current_player
+                break
+        self.switch_player()
+
+    def _get_obs(self):
+        # Transform the board into a numerical array for the observation space
+        obs = []
+        for row in self.board:
+            obs_row = []
+            for cell in row:
+                if cell == ' ':
+                    obs_row.append(0)
+                elif cell == 'X':
+                    obs_row.append(1)
+                else:  # 'O'
+                    obs_row.append(2)
+            obs.append(obs_row)
+        return obs
+
+    def _is_valid_action(self, action):
+        return self.board[0][action] == ' '
+
+    def _is_board_full(self):
+        return all(cell != ' ' for row in self.board for cell in row)
+
 
     def display_board(self):
         print(" 0 1 2 3 4 5 6")
@@ -16,53 +85,53 @@ class Connect4:
                 return True
         return False
     
-    def check_horizontal(self):
+    def check_horizontal(self,player):
         # Check horizontal
         for row in range(6):
             for col in range(4):
-                if self.board[row][col] == self.current_player and \
-                   self.board[row][col+1] == self.current_player and \
-                   self.board[row][col+2] == self.current_player and \
-                   self.board[row][col+3] == self.current_player:
+                if self.board[row][col] == player and \
+                   self.board[row][col+1] == player and \
+                   self.board[row][col+2] == player and \
+                   self.board[row][col+3] == player:
                     return True
                 
-    def check_vertical(self): 
+    def check_vertical(self,player): 
         # Check vertical
         for col in range(7):
             for row in range(3):
-                if self.board[row][col] == self.current_player and \
-                   self.board[row+1][col] == self.current_player and \
-                   self.board[row+2][col] == self.current_player and \
-                   self.board[row+3][col] == self.current_player:
+                if self.board[row][col] == player and \
+                   self.board[row+1][col] == player and \
+                   self.board[row+2][col] == player and \
+                   self.board[row+3][col] == player:
                     return True   
     
-    def check_positive_horizontal(self): 
+    def check_positive_horizontal(self,player): 
         # Check positive diagonal
         for col in range(4):
             for row in range(3, 6):
-                if self.board[row][col] == self.current_player and \
-                   self.board[row-1][col+1] == self.current_player and \
-                   self.board[row-2][col+2] == self.current_player and \
-                   self.board[row-3][col+3] == self.current_player:
+                if self.board[row][col] == player and \
+                   self.board[row-1][col+1] == player and \
+                   self.board[row-2][col+2] == player and \
+                   self.board[row-3][col+3] == player:
                     return True
                 
-    def check_negative_horizontal(self): 
+    def check_negative_horizontal(self,player): 
         # Check negative diagonal
         for col in range(4):
             for row in range(3):
-                if self.board[row][col] == self.current_player and \
-                   self.board[row+1][col+1] == self.current_player and \
-                   self.board[row+2][col+2] == self.current_player and \
-                   self.board[row+3][col+3] == self.current_player:
+                if self.board[row][col] == player and \
+                   self.board[row+1][col+1] == player and \
+                   self.board[row+2][col+2] == player and \
+                   self.board[row+3][col+3] == player:
                     return True
         
 
-    def check_winner(self):
+    def check_winner(self,player):
  
-        if self.check_horizontal() or \
-            self.check_vertical() or \
-            self.check_positive_horizontal() or \
-            self.check_negative_horizontal():
+        if self.check_horizontal(player) or \
+            self.check_vertical(player) or \
+            self.check_positive_horizontal(player) or \
+            self.check_negative_horizontal(player):
             return True
         
         return False
@@ -70,30 +139,49 @@ class Connect4:
     def switch_player(self):
         self.current_player = 'O' if self.current_player == 'X' else 'X'
 
-    def play_game(self):
-        while not self.game_over:
-            self.display_board()
+def play_game(env):
+    _ = env.reset()
+    done = False
+
+    while not done:
+        env.render()
+        valid_move = False
+
+        while not valid_move:
             try:
-                column = int(input(f"Player {self.current_player}, choose a column (0-6): "))
-                if column < 0 or column > 6:
+                action = int(input(f"Player {env.current_player}, choose a column (0-6): "))
+                if action < 0 or action > 6:
                     raise ValueError
+
+                # Check if the column is full
+                if env.board[0][action] != ' ':
+                    print("Column is full. Try a different one.")
+                else:
+                    valid_move = True
+
             except ValueError:
                 print("Invalid column. Try again.")
-                continue
 
-            if not self.make_move(column):
-                print("Column is full. Try a different one.")
-                continue
+        _, reward, done, info = env.step(action)
 
-            if self.check_winner():
-                self.display_board()
-                print(f"Player {self.current_player} wins!")
-                self.game_over = True
+        if done:
+            env.render()
+            if reward == 1:
+                print(f"Player {env.current_player} wins!")
             else:
-                self.switch_player()
+                print("Game over!")
 
-        print("Game over!")
+def random_agent_test(env,render=True):
+    _ = env.reset()
+    done = False
+    while not done:
+        if render:
+            env.render()
+        action = random.choice([i for i in range(7) if env._is_valid_action(i)])
+        _, _, done, _ = env.step(action)
+    assert done  # Ensure the game ends
 
 if __name__ == "__main__":
-    game = Connect4()
-    game.play_game()
+    env = Connect4Env()
+    # play_game(env)
+    random_agent_test(env)
